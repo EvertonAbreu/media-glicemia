6fabd4f32331659d4c17461a1d8bfbaf515d9acd
EvertonAbreu <90807152+EvertonAbreu@users.noreply.github.com>
2026-06-07 21:29:51 -0300

    Refactor Firebase config and export default app

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,7 @@
-
-// Importar os SDKs do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-// Configuração do Firebase - Substitua com seus dados se necessário
 const firebaseConfig = {
     apiKey: "AIzaSyCelgv6HUBqAKkQeKBNDdaP5kakx9-pCZs",
     authDomain: "diabcare-ff19f.firebaseapp.com",
@@ -14,10 +11,7 @@ const firebaseConfig = {
     appId: "1:667128255049:web:861eeec8a96169e380a598"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportar serviços para uso em outros arquivos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
